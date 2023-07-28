c0592d57ca387db07fc9347be3eafbc348e0c08b
Sadia-Tasfia <tasfia.sujana2903@gmail.com>
2023-07-28 22:52:41 +0600

    second commit

--- app.js
+++ app.js
@@ -21,3 +21,49 @@ document.getElementById("generate-pin").addEventListener('click',function(){
   const displayPinField = document.getElementById('display-pin');
   displayPinField.value = pin;
 });
+
+document.getElementById("calculator").addEventListener('click', function(event){
+  const number = event.target.innerText;
+  const typeNumberField = document.getElementById("typed-number");
+  const previousNumber = typeNumberField.value;
+
+  // console.log(number);
+  if(isNaN(number)){
+    if(number === 'C'){
+      typeNumberField.value = '';
+    }
+    else if(number === "<") {
+      const digits = previousNumber.split('');
+      digits.pop();
+
+      const remainingDigit = digits.join('');
+      typeNumberField.value = remainingDigit;
+    }
+    } 
+  else{
+    const newNumber = previousNumber + number;
+    typeNumberField.value = newNumber;
+  }
+});
+
+document.getElementById('verify-btn').addEventListener('click',function(){
+  const displayPinField = document.getElementById('display-pin');
+  const currentPin = displayPinField.value;
+  
+  const typeNumberField = document.getElementById("typed-number");
+  const typeNumber =typeNumberField.value;
+
+  const pinMessage = document.getElementById("pin-matched");
+  const pinFailureMessage = document.getElementById("pin-failure");
+  
+  if(typeNumber === currentPin){
+    pinMessage.style.display = 'block';
+    pinFailureMessage.style.display = "none";
+  }
+  else{
+    
+    pinFailureMessage.style.display= 'block';
+    pinMessage.style.display = "none";
+  }
+  
+});
